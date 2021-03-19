ae2caeb923ce467836bd9cd1e54a4e4842876e65
reinaldo luiz <reinaldolluiz@gmail.com>
2021-03-18 22:26:35 -0300

    Parte 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,28 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
 import {ClienteInserirComponent} from "./clientes/cliente-inserir/cliente-inserir.component";
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CabecallhoComponent } from './cabecallho/cabecallho.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClienteInserirComponent
+    ClienteInserirComponent,
+    CabecallhoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cabecallho/cabecallho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecallho',
+  templateUrl: './cabecallho.component.html',
+  styleUrls: ['./cabecallho.component.css']
+})
+export class CabecallhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
